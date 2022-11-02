d508eff7647429fa6c5f5b22b99d82865e5375ca
Harry <harald.innetzberger@web.de>
2022-11-02 08:55:29 +0100

    Setup register and login form

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 
 const User = require('../models/userModel')
-const { restart } = require('nodemon')
 
 // @desc Register new user
 // @route POST /api/users
